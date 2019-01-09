6e505cf9bdfe0499f882bebcde28eb309861a7f9
wailo <wailo_2@hotmail.com>
2019-01-09 18:50:23 +0100

    Added package header

--- poet_api.el
+++ poet_api.el
@@ -1,3 +1,30 @@
+;;; package-name.el --- Package description (don't include the word "Emacs")  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019 W.Yahia
+
+;; Author: W.Yahia
+;; URL: https://github.com/wailo/emacs-poet
+;; Version: 0.1-pre
+;; Package-Requires: ((emacs "25.2"))
+;; Keywords: Po.et
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; code:
 
 (defcustom poet-api-url "https://api.poetnetwork.net/works" "POET API URL"
